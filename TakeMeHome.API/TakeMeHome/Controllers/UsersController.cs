962038fb1830b37ea24166a2bedda7481dc5a2bf
Ivanovich0705 <ivantenis1@icloud.com>
2022-10-30 01:28:36 -0500

    feat(Controller): Implemented User Controller.

---
+++ TakeMeHome.API/TakeMeHome/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using TakeMeHome.API.TakeMeHome.Domain.Models;
+using TakeMeHome.API.TakeMeHome.Domain.Services;
+using TakeMeHome.API.TakeMeHome.Resources;
+
+namespace TakeMeHome.API.TakeMeHome.Controllers;
+
+[Route("/api/v1/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly IUserService _userService;
+    private readonly IMapper _mapper;
+
+    public UsersController(IUserService userService, IMapper mapper)
+    {
+        _userService = userService;
+        _mapper = mapper;
+    }
+    
+    [HttpGet]
+    public async Task<IEnumerable<UserResource>> GetAllAsync()
+    {
+        var users = await _userService.ListAsync();
+        var resources = _mapper.Map<IEnumerable<User>, IEnumerable<UserResource>>(users);
+        return resources;
+    }
+}
