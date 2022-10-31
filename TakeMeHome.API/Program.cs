9c03b51028068413b101db2b4605642aebddaf8e
Ivanovich0705 <ivantenis1@icloud.com>
2022-10-31 02:43:13 -0500

    chore(Mapping)

--- TakeMeHome.API/Program.cs
+++ TakeMeHome.API/Program.cs
@@ -1,5 +1,13 @@
+using System.Configuration;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.EntityFrameworkCore;
+using TakeMeHome.API.TakeMeHome.Domain.Models;
+using TakeMeHome.API.TakeMeHome.Domain.Repositories;
+using TakeMeHome.API.TakeMeHome.Domain.Services;
+using TakeMeHome.API.TakeMeHome.Mapping;
 using TakeMeHome.API.TakeMeHome.Persistence.Contexts;
+using TakeMeHome.API.TakeMeHome.Persistence.Repositories;
+using TakeMeHome.API.TakeMeHome.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,8 +19,29 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 //Add Database Connection
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection")));
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(
+        options => options.UseMySQL(connectionString)
+            .LogTo(Console.WriteLine, LogLevel.Information)
+            .EnableSensitiveDataLogging()
+            .EnableDetailedErrors());
+
+//Add lower case routes
+builder.Services.AddRouting(options=> options.LowercaseUrls = true);
+
+//Dependency Injection Configuration
+builder.Services.AddScoped<IUserRepository, CategoryRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<IOrderStatusRepository, OrderStatusRepository>();
+builder.Services.AddScoped<IOrderStatusService, OrderStatusService>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+//AutoMapper Configuration
+builder.Services.AddAutoMapper(
+    typeof(ModelToResourceProfile), 
+    typeof(ResourceToModelProfile));
 
 var app = builder.Build();
 
