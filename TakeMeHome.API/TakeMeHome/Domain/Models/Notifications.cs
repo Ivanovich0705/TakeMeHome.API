b7589ade9c13fc76eaeaf72f73e27f1f42f0b295
FaridIzquierdo <u202015215@upc.edu.pe>
2022-11-09 01:38:33 -0500

    Feat(notifications)
     Created Notifications Model.

---
+++ TakeMeHome.API/TakeMeHome/Domain/Models/Notifications.cs
@@ -0,0 +1,15 @@
+﻿namespace TakeMeHome.API.TakeMeHome.Domain.Models;
+
+public class Notifications
+{
+    public int Id { get; set; }
+    
+    public string Type { get; set; }
+    
+    
+    //Relationships
+    
+    public int UserId { get; set; }
+    
+    public User User { get; set; }
+}
